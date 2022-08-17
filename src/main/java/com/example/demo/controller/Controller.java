938515546e61799b803c819710eaf621bdcd19cf
Saloni Patil <Saloni Patil@DESKTOP-F71Q2K6>
2022-08-17 17:44:55 +0530

    initial commit

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+//import java.util.ArrayList;
+//import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -14,14 +14,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Customer;
@@ -32,7 +30,7 @@ import com.example.demo.ServiceMapper.IServiceMapper;
 
 
 @RestController
-@RequestMapping("/backend")
+//@RequestMapping("/backend")
 public class Controller {
 	
 	Logger logger = LoggerFactory.getLogger(Controller.class);
